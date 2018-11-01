3a64b89d236defeb8a486b58e3ef124fb3398d15
Sebas <msebastien1696@gmail.com>
2018-11-01 16:02:21 -0400

    Ajout du package DAO et de l'interface DAO de laquelle deriveront tout nos daos

---
+++ src/java/com/GestionStage/DAO/Dao.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.GestionStage.DAO;
+
+import java.util.List;
+
+/**
+ *
+ * @author sebas
+ * @param <T>
+ */
+public interface Dao<T> {
+    public boolean create(T x);
+    public T find(int key);
+    public T find(String key);
+    public List<T> findAll(T x);
+    public boolean update(T x);
+    public boolean delete(T x);
+}
