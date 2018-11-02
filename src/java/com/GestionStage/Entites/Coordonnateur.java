7dfff5890a1b349388f1b784954cca002a989070
Sebas <msebastien1696@gmail.com>
2018-11-01 22:21:34 -0400

    Ajout des classes entites utilisateurs et coordonnateur

--- src/java/com/GestionStage/Entites/Coordonnateur.java
+++ src/java/com/GestionStage/Entites/Coordonnateur.java
@@ -10,5 +10,20 @@ package com.GestionStage.Entites;
  * @author sebas
  */
 public class Coordonnateur extends Utilisateur{
+    private String id_connrdonnateur;
+
+    /**
+     * @return the id_connrdonnateur
+     */
+    public String getId_connrdonnateur() {
+        return id_connrdonnateur;
+    }
+
+    /**
+     * @param id_connrdonnateur the id_connrdonnateur to set
+     */
+    public void setId_connrdonnateur(String id_connrdonnateur) {
+        this.id_connrdonnateur = id_connrdonnateur;
+    }
     
 }
